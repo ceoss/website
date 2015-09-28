0f92b22e739a945d397c801d4356e5ddb73dcd61
Evelyn Hathaway <git@evelynhathaway.com>
2015-09-27 21:01:50 -0700

    Circle Reveal

---
+++ circle-reveal.js
@@ -0,0 +1,37 @@
+function circleRevealIni () {
+	var doc = document;
+	var elements = doc.getElementsByClassName('circle-reveal');
+	for (var i = elements.length - 1; i >= 0; i--) {
+		elements[i].addEventListener("mouseover", function(event) {
+			var ele = event.target;
+			while (true) {
+				if (ele.classList.contains("circle-reveal")) {
+					break;
+				};
+				ele = ele.parentElement;
+			}
+			for (var i = ele.children.length - 1; i >= 0; i--) {
+				if (ele.children[i].classList.contains("circle-reveal-content")) {
+					ele.children[i].classList.add("circle-reveal-active");
+					break;	
+				};
+			};
+		}, false);
+		elements[i].addEventListener("mouseleave", function(event) {
+			var ele = event.target;
+			while (true) {
+				if (ele.classList.contains("circle-reveal")) {
+					break;
+				};
+				ele = ele.parentElement;
+			}
+			for (var i = ele.children.length - 1; i >= 0; i--) {
+				if (ele.children[i].classList.contains("circle-reveal-content")) {
+					ele.children[i].classList.remove("circle-reveal-active");
+					break;	
+				};
+			};
+		}, false);
+	};
+}
+circleRevealIni();
