9fb046973206299388c07688a417263eb337ec39
Evelyn Hathaway <git@evelynhathaway.com>
2015-10-02 18:45:55 -0700

    Circle Reveal Update, Remove Listeners After Use

--- js/circle-reveal/circle-reveal.js
+++ js/circle-reveal/circle-reveal.js
@@ -6,7 +6,7 @@ function circleRevealIni () {
 	var doc = document;
 	var elements = doc.getElementsByClassName('circle-reveal');
 	for (var i = elements.length - 1; i >= 0; i--) {
-		elements[i].addEventListener("mouseover", function(event) {
+		elements[i].addEventListener("mouseover", function mouseover(event) {
 			var ele = event.target;
 			while (true) {
 				if (ele.classList.contains("circle-reveal")) {
@@ -17,18 +17,17 @@ function circleRevealIni () {
 			for (var i = ele.children.length - 1; i >= 0; i--) {
 				if (ele.children[i].classList.contains("circle-reveal-content")) {
 					ele.children[i].classList.add("circle-reveal-active");
-					// Link Cards
 					/* For mobile tapping support.
-					   May replace with onclick and ontap.
-					 */
-					ele.children[i].addEventListener("transitionend", function(event) {
+					   May replace with onclick and ontap.*/
+					ele.children[i].addEventListener("transitionend", function transitionendactive(event) {
+						event.target.removeEventListener("transitionend", transitionendactive, false);
 						event.target.style.pointerEvents = "auto";
 					}, false);
 					break;	
 				};
 			};
 		}, false);
-		elements[i].addEventListener("mouseleave", function(event) {
+		elements[i].addEventListener("mouseleave", function mouseleave(event) {
 			var ele = event.target;
 			while (true) {
 				if (ele.classList.contains("circle-reveal")) {
@@ -39,11 +38,10 @@ function circleRevealIni () {
 			for (var i = ele.children.length - 1; i >= 0; i--) {
 				if (ele.children[i].classList.contains("circle-reveal-content")) {
 					ele.children[i].classList.remove("circle-reveal-active");
-						// Link Cards
 						/* For mobile tapping support.
-						   May replace with onclick and ontap.
-						 */
-						ele.children[i].addEventListener("transitionend", function(event) {
+						   May replace with onclick and ontap.*/
+						ele.children[i].addEventListener("transitionend", function transitionend(event) {
+							event.target.removeEventListener("transitionend", transitionend, false);
 							event.target.style.pointerEvents = "none";
 						}, false);
 					break;	
