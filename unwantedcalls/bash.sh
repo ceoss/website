71373bca5daec73fd4f96419a78ee44b89bbdf5b
Evelyn Hathaway <git@evelynhathaway.com>
2015-10-23 20:19:15 -0700

    Rename Folder, Bash Program

---
+++ unwantedcalls/bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# cron job: /etc/cron.daily/unwantedcalls
+wget -O data.html https://consumercomplaints.fcc.gov/hc/en-us/articles/20523944$
+link="https://consumercomplaints.fcc.gov$(grep -oh "/.\+\.csv" data.html)"
+mkdir -p ./old
+time=$(date +"%m_%d_%Y")
+mv data.html "./old/data_$time.html"
+mv data.csv "./old/data_$time.csv"
+wget -O data.csv $link --no-check-certificate
