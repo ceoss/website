6992b44d34591237383f609f2c45a7c5cb00c9da
Evelyn Hathaway <git@evelynhathaway.com>
2016-10-16 22:54:00 -0700

    Removed Unwanted Calls
    Will be patched into to the graveyard repo.

--- unwantedcalls/bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# cron job: /etc/cron.daily/unwantedcalls
-wget -O data.html https://consumercomplaints.fcc.gov/hc/en-us/articles/205239443-Data-on-Unwanted-Calls --no-check-certificate
-link="https://consumercomplaints.fcc.gov$(grep -oh "/.\+\.csv" data.html)"
-mkdir -p ./old
-time=$(date +"%m_%d_%Y")
-mv data.html "./old/data_$time.html"
-mv data.csv "./old/data_$time.csv"
-wget -O data.csv $link --no-check-certificate
