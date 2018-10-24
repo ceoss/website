1140200434855b38cd429b7378cecc79f21ed18b
Evelyn Hathaway <git@evelynhathaway.com>
2018-10-24 11:57:01 -0700

    Clean slate

--- pull.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo shell_exec("cd /var/www/xor.tech/public_html/ && git fetch --all && git reset --hard origin/master && git pull && cd /var/www/xor.tech/public_html/OS-Web-Components && git fetch --all && git reset --hard origin/master && git pull")
-?>
